70bb3c2de52581b8da7db0c96ffe52c7f94e4128
Rodion Smilovskyi <smelovsky86@gmail.com>
2024-09-02 17:03:11 -0400

    updated aws launcher

--- train_aws.py
+++ train_aws.py
@@ -1,49 +1,42 @@
+import os
 import boto3
 import sagemaker
-from datetime import datetime
 from sagemaker.estimator import Estimator
 from sagemaker.debugger import TensorBoardOutputConfig
-import os
 
-role = "arn:aws:iam::905418352696:role/SageMakerFullAccess"
+ROLE = "arn:aws:iam::905418352696:role/SageMakerFullAccess"
+BASE_JOB_NAME = "reinforce-500-1"
+
 boto_session = boto3.Session(
     profile_name="905418352696_AdministratorAccess", region_name="us-east-1"
 )
+
 sagemaker_session = sagemaker.Session(boto_session=boto_session)
 
-base_job_name = "reinforce"
-
-
-date_str = datetime.now().strftime("%d-%m-%Y")
-time_str = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
-job_name = f"{base_job_name}-{time_str}"
-
-s3_output_bucket = os.path.join("s3://", sagemaker_session.default_bucket(), job_name)
-
 tensorboard_output_config = TensorBoardOutputConfig(
-    s3_output_path=os.path.join(s3_output_bucket, "tensorboard"),
+    s3_output_path=os.path.join("s3://", sagemaker_session.default_bucket()),
     container_local_output_path="/opt/ml/output/tensorboard",
 )
 
 estimator = Estimator(
     sagemaker_session=sagemaker_session,
-    image_uri="905418352696.dkr.ecr.us-east-1.amazonaws.com/ai-repo:reinforce_discrete.4e6148d",
-    role=role,
+    image_uri="905418352696.dkr.ecr.us-east-1.amazonaws.com/ai-repo:reinforce_discrete.cc28b05",
+    role=ROLE,
     max_run=24 * 60 * 60,
-    base_job_name=base_job_name,
+    base_job_name=BASE_JOB_NAME,
     instance_count=1,
     container_arguments=[
         "train",
         "--episodes",
-        "11",
+        "500",
         "--episode-trigger-step",
-        "5",
+        "10",
         "--neurons",
-        "128",
+        "100",
         "--batch-size",
         "128",
         "--buffer-size",
-        "250",
+        "2000",
         "--prefix",
         "/opt/ml",
     ],
@@ -54,4 +47,4 @@ estimator = Estimator(
 
 estimator.fit(wait=False)
 
-print(f"Job {job_name} submitted")
+print(f"Job {BASE_JOB_NAME} submitted")
