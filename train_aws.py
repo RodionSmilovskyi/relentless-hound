a60b621b46e72f15db2c15e716e0a2b8f98972fa
Rodion Smilovskyi <smelovsky86@gmail.com>
2024-09-03 11:55:20 -0400

    Updated list of hyperparameters

--- train_aws.py
+++ train_aws.py
@@ -12,31 +12,29 @@ boto_session = boto3.Session(
 )
 
 sagemaker_session = sagemaker.Session(boto_session=boto_session)
+default_path = os.path.join("s3://", sagemaker_session.default_bucket())
 
 tensorboard_output_config = TensorBoardOutputConfig(
-    s3_output_path=os.path.join("s3://", sagemaker_session.default_bucket()),
+    s3_output_path=default_path,
     container_local_output_path="/opt/ml/output/tensorboard",
 )
 
 estimator = Estimator(
     sagemaker_session=sagemaker_session,
-    image_uri="905418352696.dkr.ecr.us-east-1.amazonaws.com/ai-repo:reinforce_discrete.cc28b05",
+    image_uri="905418352696.dkr.ecr.us-east-1.amazonaws.com/ai-repo:reinforce_discrete.6505b20",
     role=ROLE,
     max_run=24 * 60 * 60,
     base_job_name=BASE_JOB_NAME,
     instance_count=1,
+    checkpoint_s3_uri=os.path.join(default_path, 'checkpoints', BASE_JOB_NAME),
     container_arguments=[
         "train",
         "--episodes",
-        "500",
-        "--episode-trigger-step",
-        "10",
-        "--neurons",
         "100",
-        "--batch-size",
-        "128",
+        "--validation-episode",
+        "5",
         "--buffer-size",
-        "2000",
+        "200",
         "--prefix",
         "/opt/ml",
     ],
