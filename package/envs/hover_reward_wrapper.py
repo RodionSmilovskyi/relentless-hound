13926b38c4ebea07820f14135aba4268053bcf34
Rodion Smilovskyi <smelovsky86@gmail.com>
2024-09-13 19:00:51 -0400

    simplified reward system

--- package/envs/hover_reward_wrapper.py
+++ package/envs/hover_reward_wrapper.py
@@ -13,17 +13,18 @@ class HowerRewardWrapper(gym.Wrapper):
         super().__init__(env)
 
         self.hover_altitude = HOVER_ALTITUDE / MAX_ALTITUDE
+        self.entered_range = False
 
     def step(self, action: ActType):
         obs, reward, terminated, info = super().step(action)
         altitude = obs[5]
         above_ground = altitude > MIN_ALTITUDE / MAX_ALTITUDE
 
-        if above_ground:
-            reward = reward + 1
-
         if abs(altitude - self.hover_altitude) < self.hover_altitude * 0.1:
-            reward = reward + 5
+            reward = 1
+            self.entered_range = True
+        elif self.entered_range is True:
+            terminated = True
 
         terminated = (
             True if not above_ground and info["step_number"] > 50 else terminated
