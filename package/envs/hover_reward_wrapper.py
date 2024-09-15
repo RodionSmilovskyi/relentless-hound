77054bb40918598ff3303c94639239a14dcccfec
Rodion Smilovskyi <smelovsky86@gmail.com>
2024-09-15 15:38:53 -0400

    Fixed several issues

--- package/envs/hover_reward_wrapper.py
+++ package/envs/hover_reward_wrapper.py
@@ -12,19 +12,26 @@ class HowerRewardWrapper(gym.Wrapper):
     def __init__(self, env: gym.Env):
         super().__init__(env)
 
-        self.hover_altitude = HOVER_ALTITUDE / MAX_ALTITUDE
+        self.hover_altitude = round(HOVER_ALTITUDE / MAX_ALTITUDE, 4)
         self.entered_range = False
+        self.prev_altitude_diff = self.hover_altitude
 
     def step(self, action: ActType):
         obs, reward, terminated, info = super().step(action)
-        altitude = obs[5]
+        altitude = round(obs[5], 4)
+        reward = -1
+        current_altitude_diff = round(abs(altitude - self.hover_altitude), 4)
         above_ground = altitude > MIN_ALTITUDE / MAX_ALTITUDE
 
-        if abs(altitude - self.hover_altitude) < self.hover_altitude * 0.1:
-            reward = 1
-            self.entered_range = True
-        elif self.entered_range is True:
-            terminated = True
+        if current_altitude_diff < self.hover_altitude * 0.1:
+            reward = reward + 10
+        else:
+            if current_altitude_diff > self.prev_altitude_diff:
+                reward = reward - 5
+            elif current_altitude_diff < self.prev_altitude_diff:
+                reward = reward + 5
+
+        self.prev_altitude_diff = current_altitude_diff
 
         terminated = (
             True if not above_ground and info["step_number"] > 50 else terminated
