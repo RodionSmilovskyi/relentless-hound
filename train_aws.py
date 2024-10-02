4a6c4ca5bf37cd77c5351bf8a0da4e36fa082644
Rodion Smilovskyi <smelovsky86@gmail.com>
2024-10-02 17:37:18 -0400

    Added video recording

--- train_aws.py
+++ train_aws.py
@@ -1,53 +1,33 @@
-from datetime import datetime
 import os
 import boto3
 import sagemaker
 from sagemaker.estimator import Estimator
 from sagemaker.debugger import TensorBoardOutputConfig
 
+ROLE = "arn:aws:iam::905418352696:role/SageMakerFullAccess"
+BASE_JOB_NAME = "pytorch-dqn-cartpole"
 
-role = "arn:aws:iam::905418352696:role/SageMakerFullAccess"
 boto_session = boto3.Session(
     profile_name="905418352696_AdministratorAccess", region_name="us-east-1"
 )
+
 sagemaker_session = sagemaker.Session(boto_session=boto_session)
-
-base_job_name = "hover-128-128-10000-5000"
-
-
-date_str = datetime.now().strftime("%d-%m-%Y")
-time_str = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
-job_name = f"{base_job_name}-{time_str}"
-
-s3_output_bucket = os.path.join("s3://", sagemaker_session.default_bucket(), job_name)
+default_path = os.path.join("s3://", sagemaker_session.default_bucket())
 
 tensorboard_output_config = TensorBoardOutputConfig(
-    s3_output_path=os.path.join(s3_output_bucket, "tensorboard"),
+    s3_output_path=default_path,
     container_local_output_path="/opt/ml/output/tensorboard",
 )
 
 estimator = Estimator(
     sagemaker_session=sagemaker_session,
-    image_uri="905418352696.dkr.ecr.us-east-1.amazonaws.com/ai-repo:hover.592c16b",
-    role=role,
-    max_run=24 * 60 * 60,
-    base_job_name=base_job_name,
+    image_uri="905418352696.dkr.ecr.us-east-1.amazonaws.com/ai-repo:pytorch_dqn_cartpole.b78edef",
+    role=ROLE,
+    max_run=1 * 60 * 60,
+    base_job_name=BASE_JOB_NAME,
     instance_count=1,
-    container_arguments=[
-        "train",
-        "--episodes",
-        "5001",
-        "--episode-trigger-step",
-        "100",
-        "--neurons",
-        "128",
-        "--batch-size",
-        "128",
-        "--buffer-size",
-        "10000",
-        "--prefix",
-        "/opt/ml",
-    ],
+    checkpoint_s3_uri=os.path.join(default_path, "checkpoints", BASE_JOB_NAME),
+    container_arguments=["train", "--prefix", "/opt/ml", "--use-virtual-display"],
     tensorboard_output_config=tensorboard_output_config,
     instance_type="ml.g4dn.2xlarge",
 )
@@ -55,4 +35,4 @@ estimator = Estimator(
 
 estimator.fit(wait=False)
 
-print(f"Job {job_name} submitted")
+print(f"Job {BASE_JOB_NAME} submitted")
--- train_local.sh
+++ train_local.sh
@@ -12,7 +12,7 @@ rm -rf $(pwd)/test_dir/model/*
 rm -rf $(pwd)/test_dir/output/*
 
 # Capture the output of the docker run command
-output=$(docker run -v $(pwd)/test_dir:/opt/ml --rm ${image} train --prefix /opt/ml)
+output=$(docker run -v $(pwd)/test_dir:/opt/ml --rm ${image} train --prefix /opt/ml --use-virtual-display)
 
 # Print the output for verification
 echo "$output"
